df5350e02a5ff805d1e6f83a4eb9e024896e064d
WHOISshuvam <adhikarishuvam450@gmail.com>
2023-02-12 19:35:10 +0545

    quizapp

--- quiz project/main.go
+++ quiz project/main.go
@@ -15,5 +15,37 @@ func main() {
 	var age uint
 	fmt.Scan(&age)
 
-	fmt.Println(age >= 10)
+	if age >= 10 {
+		fmt.Println("You are eligible to play")
+	} else {
+		fmt.Println("You can't play")
+		return
+	}
+
+	score := 0
+
+	fmt.Printf("How do you see hidden character in text file in linux? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+	fmt.Println(answer, answer2)
+
+	if answer+" "+answer2 == "cat -v" {
+		fmt.Println("Correct")
+
+	} else if answer+" "+answer2 == "Cat -v" {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Incorrect!")
+	}
+
+	fmt.Printf("What is the capital city of Nepal? ")
+	var capital string
+	fmt.Scan(&capital)
+
+	if answer == "kathmandu" {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Incorrect")
+	}
 }
